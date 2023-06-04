5ce700c3aeff5020e504355e5ef894299fa83db4
ashleyyong10 <ashleyyongwork@gmail.com>
2023-06-05 00:26:24 +0800

    added cors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 8000;
-// const cors = require("cors");
+const cors = require("cors");
 
 connectDB();
 
@@ -14,11 +14,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use(
-//   cors({
-//     origin: "https://deploytest-frontend.vercel.app",
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://deploytest-frontend.vercel.app",
+  })
+);
 
 app.get("/", (req, res, _next) => {
   res.send("<h1>AXSauce Backend Server</h1>");
